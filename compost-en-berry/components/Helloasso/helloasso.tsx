ae1093930a4d0c620753ea557d29778424f9f1c1
clementmosser <clement-mosser@live.fr>
2025-01-28 11:01:43 +0100

    Modififcation du lien Helloasso

--- compost-en-berry/components/Helloasso/helloasso.tsx
+++ compost-en-berry/components/Helloasso/helloasso.tsx
@@ -18,7 +18,7 @@ export default function Helloasso() {
         <div className="flex items-center justify-center pt-4 pb-10 bg-helloasso">
         <iframe 
             id="haWidget" 
-            src="https://www.helloasso.com/associations/compost-en-berry/adhesions/adhesion-2023-2024/widget" 
+            src="https://www.helloasso.com/associations/compost-en-berry/adhesions/adhesion2025/widget" 
             className="w-4/5 h-screen">
         </iframe>
         </div>
