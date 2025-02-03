e1aa9516eb2b6ea401f57c14ebb6834c30dce759
clementmosser <clement-mosser@live.fr>
2025-02-03 15:47:27 +0100

    Addind list numbers to legal mentions

--- compost-en-berry/components/LegalMentions/legalMentions.tsx
+++ compost-en-berry/components/LegalMentions/legalMentions.tsx
@@ -18,17 +18,20 @@ export default function LegalMentions() {
       }, [])
   
   return (
-    <div className="px-8 py-8 h-fit min-h-screen bg-beige-berry-100">
+    <div className="px-10 py-8 h-fit min-h-screen bg-beige-berry-100">
         <div className="ml-2 sm:ml-8 w-11/12 md:w-3/4">
-            {legalMentions.map((legalMention, index) => (
-                <ol key={`LG: + ${index}`}>
-                    <li className='text-base md:text-lg font-semibold py-4'>{legalMention.title}</li>
-                    {legalMention.content?.map((content, index) => (
-                        <p key={`Content: + ${index}`} className='text-xs md:text-normal '>{content}</p>
-                    ))}
-                </ol>
-                
-            ))}
+            <ol className="list-decimal">
+                {legalMentions.map((legalMention, index) => (
+                    <div key={`LG: + ${index}`}>
+                        <li className='text-base md:text-lg font-semibold py-4'>
+                            {legalMention.title}
+                        </li>
+                        {legalMention.content?.map((content, index) => (
+                            <p key={`Content: + ${index}`} className='text-xs md:text-normal '>{content}</p>
+                        ))}
+                    </div>
+                ))}
+            </ol>
         </div>
     </div>
   )
